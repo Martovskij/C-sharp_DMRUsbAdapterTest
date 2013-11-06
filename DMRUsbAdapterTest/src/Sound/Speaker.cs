7c1499421ac98d9141febce227f59fe12c382ad8
Martovskij <mcgate@yandex.ru>
2013-11-06 09:24:24 +0300

    added  "speaker" class added play  captured sound added mic bar on mainwindow

---
+++ DMRUsbAdapterTest/src/Sound/Speaker.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DMRUsbAdapterTest.src.Sound
+{
+    class Speaker 
+    {
+
+        NAudio.Wave.DirectSoundOut waveOut = null;
+        public int DeviceIndex { get; set; }
+        NAudio.Wave.WaveInProvider waveIn = null;
+
+
+        public Speaker()
+        {
+            
+        }
+
+
+        public void PlayStream(NAudio.Wave.WaveIn sourceStream)
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+            if (sourceStream == null) return;
+            waveOut = new NAudio.Wave.DirectSoundOut();
+            waveIn = new NAudio.Wave.WaveInProvider(sourceStream);
+            waveOut.Init(waveIn);
+            waveOut.Play();
+        }
+
+
+
+
+    }
+}
